73ecb9061b38f97440469040f790402922b70ecf
JimNotCarry <59293465+JimNotCarry@users.noreply.github.com>
2021-04-15 16:12:48 +0200

    PaymentStart

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 
 import React from 'react'
-import {Browserrouter as Router,Route, Link, Switch } from 'react-router';
+import {BrowserRouter as Router,Route, Link, Switch } from 'react-router-dom';
 import Kassa from './Components/Kassa'
 import Listing from './Components/Listing'
+import Subscribe from './Components/Subscribe'
+import Confirm from './Components/Confirm'
 import Form from './Components/Form'
 import Payment from './Components/Payment'
 import { useState } from 'react'
-
-const initialValues  = {
-    email: '',
-    Nyhetsbrev: '',
-    firstName: '',
-    lastName: '',
-    Adress: '',
-    Lgh: '',
-    Postnummer: '',
-    Ort: '',
-    land: '',
-    telefonnummer: '',
-    creditcard: '',
-};
-
-const [costumerInfo, setCostumerInfo]  = useState(initialValues);
 
 const home = () => {
     <div>
@@ -36,15 +22,31 @@ const home = () => {
 }
 
 const App = () => {
+
+    
+const initialValues  = {
+    email: '',
+    Nyhetsbrev: '',
+    firstName: '',
+    lastName: '',
+    Adress: '',
+    Lgh: '',
+    Postnummer: '',
+    Ort: '',
+    land: '',
+    telefonnummer: '',
+    creditcard: '',
+};
+
+    const [costumerInfo, setCostumerInfo]  = useState(initialValues);
+
     return (
 <Router>
-<div>
     <Switch>
-        <Route path='/' exact component={home}/>
-        <Route path='/Kassa' exact><Kassa setCostumerInfo = {setCostumerInfo} /> Kassa</Route>
-        <Route path='/Order' exact><Listing costumerInfo = {costumerInfo} />Order</Route>
+        <Route path='/' exact component={Subscribe}/>
+        <Route path='/Kassa' exact><Kassa setCostumerInfo = {setCostumerInfo} /></Route>
+        <Route path='/Confirm' exact><Confirm costumerInfo = {costumerInfo} /></Route>
     </Switch>
-    </div>;
 </Router>
     )
 }
--- src/Components/Subscribe.js
+++ src/Components/Subscribe.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Subscribe = () => {
     return (
         <div>
         <h1 id="ConfirmH1">Naked Wine Lovers</h1>
 
+        <Link to="/Kassa">
         <button type="button" id="Subscribeknapp">Prenumerera</button>
+        </Link>
         </div>
     )
 }
