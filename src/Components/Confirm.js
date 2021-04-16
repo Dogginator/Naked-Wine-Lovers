779c552f2e296a9693958bd082000ae6d1c35111
Dogginator <douglas.kvarnstrom91@gmail.com>
2021-04-16 10:22:44 +0200

    lets see if this dose it

--- src/Components/Confirm.js
+++ src/Components/Confirm.js
@@ -1,7 +1,7 @@
 import { GrDeliver } from 'react-icons/gr';
 import { Link } from 'react-router-dom'
 
-export const Confirm = () => {
+export const Confirm = ({setValue}) => {
     const Line = ({ color }) => (
         <hr
             style={{
@@ -32,7 +32,7 @@ return (
                          </p>
                     <Line color="black" />
                 <div className="bekräftelseInfo">
-                <p id="p1">Håll utkik efter ditt bekräftelsemail som har skickats till din e-post {this.props.value}
+                <p id="p1">Håll utkik efter ditt bekräftelsemail som har skickats till din e-post {setValue}
                            </p>
                 <p id="p2">Vi meddlar dig via e-post när dina varor har skickats.  <br />
                 Vi hoppas att du ska bli nöjd!</p>
