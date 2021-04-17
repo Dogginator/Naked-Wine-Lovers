2eb2bff84bee72a0ca1ba0c1a715f9e65471d171
Dogginator <douglas.kvarnstrom91@gmail.com>
2021-04-17 13:49:58 +0200

    WineBar Commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,38 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
 return (
-<<<<<<< HEAD
-    <ul className="TopNavbar">
-        <div className="PosLeftNavbar1">
-        <a href="/">Hem</a>
-        </div>
-        <div className="PosLeftNavbar2">
-        <a href="#Vinlådor">Vinlådor</a>
-        </div>
-        <div className="PosLeftNavbar3">
-        <a href="#SaFunkarDet">Så funkar det</a>
-        </div>
-        <div className="PosLeftNavbar4">
-        <a href="#Vingårdarna">Vingårdarna</a>
-        </div>
-        <div className="IconNavbar">
-            <p>
-               <img src="ToppIcon.png" />  
-            </p>
-        </div>
-        <div className="PosRightNavbar1">
-            <a href="#OmOss">Om oss</a>
-        </div>
-        <div className="PosRightNavbar2">
-            <a href="#Villkor">Villkor</a>
-        </div>
-        <div className="PosRightNavbar3">
-            <a href="#Varukorg">Varukorg(0)</a>
-        </div>
-         
-        
-    </ul>
-=======
+
     <div className="navbarContainer">
         <ul className="TopNavbar">
             <div className="PosLeftNavbar1">
@@ -64,7 +33,6 @@ return (
             </div>        
         </ul>
     </div>
->>>>>>> master
 );
 }
 
