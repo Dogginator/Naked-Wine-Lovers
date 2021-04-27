a9fb18342f295b6d79ef298dfdbffb0e3c1f536a
Dogginator <douglas.kvarnstrom91@gmail.com>
2021-04-27 19:34:30 +0200

    2-5 ProductPages started

--- src/Components/Products/4.js
+++ src/Components/Products/4.js
@@ -1,14 +1,13 @@
-import React, { Component, useRef, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useRef } from 'react'
 import { IoIosArrowDown } from 'react-icons/io'
-const FlaskaPlatsTvå = "../Images/FlaskaPlatsTvå.png";
-const FlaskaPlatsEtt = "../Images/FlaskaPlatsEtt.png";
-const Nr3 = "../Images/Nr3.png";
+
+
 
 
 const Box4 = () => {
     const myRef = useRef(null);
     const executeScroll = () => myRef.current.scrollIntoView({ behavior: 'smooth' });
+
 
     return (
         <>
@@ -17,7 +16,7 @@ const Box4 = () => {
                     <div className="col" id="col-grid-info">
                         <div className="infoContainer" id="picturePos">
                             <p>
-                                <img className="TopPicture" src="./Images/TopImage.png" />
+                                <img className="TopPicture" src="./Images/BildBlandatLådan.png" alt="WineBox mixed"/>
                             </p>
                             <p id="ScrollDownInfo">Här kan du läsa om flaskorna!</p>
                             <button type="button" id="ReadInfo" onClick={executeScroll}><IoIosArrowDown /></button>
@@ -26,107 +25,162 @@ const Box4 = () => {
                     <div className="col" id="col-grid-info">
                         <div className="infoContainer">
 
-                            <h1>Vad fan är naturvin-lådan</h1>
-                            <p id="type">För nybörjaren</p>
-                            <h2>Prenumerera 1046 SEK/MÅNAD</h2>
+                            <h1>RÖTT</h1>
+                            <p id="type">RÖTT</p>
+                            <h2>Prenumerera 699 SEK /Månad</h2>
                             <p>
-                                Detta är den perfekta vinlådan för dig som är nybörjare och skulle vilja utforska världen av naturvin.
-                                Det finns många olika sorter och typer av naturvin, det kan vara svårt att som nybörjare veta vad du tycker om.
-                                Därför väljer vi noga ut ett antal viner som ingår i lådan.
-                                Du får nya viner varje månad så att du ska få en så mångsidig syn av alla goda naturviner som finns.
+                                Vinlådan för dig som vill prova på det bästa av alla världar. I den här lådan får du en av varje. 
+                                Det vill säga, ett vitt, ett orange, ett rött och ett mousserande. De passar alla till olika tillfällen, 
+                                men är alla lika goda! Månadens fyra gårdar är: Nonna Calbretta, Amigos, Three Season och Garden of Eden. 
                 </p>
                             <h3>Så här funkar det.</h3>
                             <p>
-                                Detta är en prenumeration där du får 6 flaskor naturvin per månaden i 4 månader.
-                                Vinerna är noga utvalda från de bästa vingårdarna. Denna produktsida är för vinerna du får i April månad.
-                                Är du sugen på att se nästa månads utbud? Kika längst ner på denna sida.
-                </p>
-                            <p id="text-bold-14">
-                                Välj att betala allt nu eller dela upp betalningen per månad.
-                                Totalpris: 4185 SEK. eller delbetala 1046 SEK i månaden. Fraktfritt och skatt ingår.
+                                Detta är en prenumeration. Prenumererar du på denna låda så får du fyra flaskor blandade viner varje månad. 
+                                Vinerna är noga utvalda från de bästa vingårdarna och byts varje månad ut. 
+                                Denna produktsida är för vinerna du får i april månad. 
+                                Nedan ser du varje flaska som kommer med i den här lådan. 
+                                Det är ingen bindningstid och du kan välja att avsluta prenumerationen när du vill. 
                 </p>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <Link to="/Kassa">
-                        <button type="button" id="VidareKnapp">Prenumerera</button>
-                    </Link>
+                <div className="row" id="ContainerWindow">
+                    <div className="col" id="ColPaddingZero"/>
+                    <div className="col" id="ColPaddingZero"><button type="button" id="VidareKnapp">Lägg till i varukorg</button></div>   
                 </div>
             </div>
-            <h1 id="LadansViner"><b>LÅDANS VINER I APRIL 2021</b></h1>
-            <div ref={myRef} >
-
-                <p>
-                    <img scr={FlaskaPlatsEtt} className="FlaskaPlatsEtt" />
-                </p>
-
+            <div className="row" id="ContainerWindow">
+                <div className="col" id="ColPaddingZero"/>
+                <div className="col" id="ColPaddingZero">
+                     <h1 className="RubrikBlandat"><b>LÅDANS VINER I APRIL 2021</b></h1>
+                </div>
+                <div className="col" id="ColPaddingZero"/>
             </div>
-            <p id="TextPos1">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus urna laoreet aliquet cras est sagittis semper.
-                Purus urna fames amet eu venenatis lobortis. Dictum libero, nullam mauris ut.
-                Tortor, consequat ac erat tristique luctus cras.
-            </p>
-            <div>
-                <img className="FlaskaPlatsTvå" scr={FlaskaPlatsTvå} />
+            <div className="row" id="ContainerWindow">
+                <div className="col" id="ColPaddingZero">
+                    <img className="Rött1" scr="./Images/Red1.png"></img>
+                </div>
+                <div className="col" ref={myRef} id="ColPaddingZero">
+                    <h2>RED QUEEN</h2><br/>
+                    <p>Rött, USA  2020</p><br/>
+                    <p>
+                    Detta vita vinet från Garden of Eden Vinyard i Kalifornien, USA. Inslag av päron, färska örter och honung. 
+                    Ett bra första steg för dig som från början gillar vitt vin, men ännu inte prövat naturvin. 
+                    Funkar perfekt till fisk eller vegetarisk. 
+                    </p><br/>
+                    <p>
+                    Flaska: 750 ml
+                    Alkoholhalt 12%
+                    Sockerhalt 10 g/l   
+                    </p><br/>
+                    <p id="InfoLink">
+                    <b>Läs mer om vingården:</b> <a href="/GardenOfEden">Garden of Eden</a>
+                    </p>
+                </div>
+                <div className="col" id="ColPaddingZero">
+                    <img className="Taste1"scr="./Images/Plats1.png"></img>
+                </div>
             </div>
-            <p id="TextPos2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus urna laoreet aliquet cras est sagittis semper.
-                Purus urna fames amet eu venenatis lobortis. Dictum libero, nullam mauris ut.
-                Tortor, consequat ac erat tristique luctus cras.
-            </p>
-            <div>
-                <img className="Nr3" scr={Nr3} />
+            <div className="row" id="ContainerWindow">
+            <div className="col" id="ColPaddingZero">
+                <img className="Rött2" scr="./Images/Red2.png"></img>
+                </div>
+                <div className="col" id="ColPaddingZero">
+                    <h2>XOXOXO</h2><br/>
+                    <p>Rött, Frankrike  2020</p><br/>
+                    <p>
+                    Ett fermenterat vitt vin från James and the Giant Grape. 
+                    Från norra Italien kommer denna otämjda rebell. Med smak av torkad frukt, 
+                    rostade nötter och job funkar det här vinet bäst till mustiga grytor och mycket kryddor.
+                    </p><br/>
+                    <p>
+                    Flaska: 750 ml
+                    Alkoholhalt 11,5%
+                    Sockerhalt 9 g/l   
+                    </p><br/>
+                    <p id="InfoLink">
+                    <b>Läs mer om vingården:</b> <a href="/JamesAndTheGiantGrape">James and the Giant Grape</a>
+                    </p>
+                </div>
+                <div className="col" id="ColPaddingZero">
+                <img className="Taste2"scr="./Images/Plats2.png"></img>
+                </div>
             </div>
-            <p id="TextPos3">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus urna laoreet aliquet cras est sagittis semper.
-                Purus urna fames amet eu venenatis lobortis. Dictum libero, nullam mauris ut.
-                Tortor, consequat ac erat tristique luctus cras.
-            </p>
-            <div>
-                <img className="Nr4" scr="./Images/Nr4.png" />
+            <div className="row" id="ContainerWindow">
+            <div className="col" id="ColPaddingZero">
+                <img className="Rött3" scr="./Images/Red3.png"></img>
+                </div>
+                <div className="col" id="ColPaddingZero">
+                    <h2>ROSES ARE RED</h2><br/>
+                    <p>Rött, Frankrike 2020</p><br/>
+                    <p>
+                    Ett sött mousserande från Three Seasons Vinyard vid den franska 
+                    riveran. Med sina toner av äppelcider, citrus och vita blommor 
+                    funkar detta bäst som apertif eller till desserter. 
+                    </p><br/>
+                    <p>
+                    Flaska: 750 ml
+                    Alkoholhalt 11,8%
+                    Sockerhalt 11 g/l   
+                    </p><br/>
+                    <p id="InfoLink">
+                    <b>Läs mer om vingården:</b> <a href="/ThreeSeasonsVinyard">Three Seasons Vinyard</a>
+                    </p>
+                </div>
+                <div className="col" id="ColPaddingZero">
+                    <img className="Taste3"scr="./Images/Plats3-4.png"></img>
+                </div>
             </div>
-            <p id="TextPos4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus urna laoreet aliquet cras est sagittis semper.
-                Purus urna fames amet eu venenatis lobortis. Dictum libero, nullam mauris ut.
-                Tortor, consequat ac erat tristique luctus cras.
-            </p>
-            <div>
-                <img className="Nr5" scr="./Images/Nr5.png" />
+            <div className="row" id="ContainerWindow">
+            <div className="col" id="ColPaddingZero">
+                    <img className="Rött4" scr="./Images/Red4.png"></img>
+                </div>
+                <div className="col" id="ColPaddingZero">
+                    <h2>GIRL IN RED</h2><br/>
+                    <p>Rött, Spanien 2020</p><br/>
+                    <p>
+                    Ett mustigt rött vin från Amigos Vinyard med inslag av mörka 
+                    körsbär, blåbär och lakrits. Perfekt till kött och fågel. 
+                    </p><br/>
+                    <p>
+                    Flaska: 750 ml
+                    Alkoholhalt 12%
+                    Sockerhalt 8 g/l   
+                    </p><br/>
+                    <p id="InfoLink">
+                    <b>Läs mer om vingården:</b> <a href="/AmigosVinyard">Amigos Vinyard</a>
+                    </p>
+                </div>
+                <div className="col" id="ColPaddingZero">
+                    <img className="Taste4"scr="./Images/Plats3-4.png"></img>
+                </div>
             </div>
-            <p id="TextPos5">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus urna laoreet aliquet cras est sagittis semper.
-                Purus urna fames amet eu venenatis lobortis. Dictum libero, nullam mauris ut.
-                Tortor, consequat ac erat tristique luctus cras.
-            </p>
-            <div>
-                <p>
-                    <img className="Nr6" scr="./Images/Nr6.png" />
-                </p>
+            <div className="row" id="ContainerWindow">
+                <div className="col" id="ColPaddingZero"><h1 id="KommandeLador"><b>KOMMANDE LÅDOR</b></h1></div>
             </div>
-            <p id="TextPos6">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus urna laoreet aliquet cras est sagittis semper.
-                Purus urna fames amet eu venenatis lobortis. Dictum libero, nullam mauris ut.
-                Tortor, consequat ac erat tristique luctus cras.
-            </p>
-            <h1 id="KommandeLador"><b>KOMMANDE LÅDOR</b></h1>
-            <h2 id="BoxMonth1">LÅDANS VINER I MAJ</h2>
-            <h2 id="BoxMonth2">LÅDANS VINER I JUNI</h2>
-            <h2 id="BoxMonth3">LÅDANS VINER I JULI</h2>
-            <div>
-                <img className="NextVine" scr="./Images/Maj.png" />
+            <div className="row" id="ContainerWindow">
+                <div className="col" id="ColPaddingZero"/>
+                <div className="col" id="ColPaddingZero"><h2 id="BoxMonth1">LÅDANS VINER I MAJ</h2></div>
+                <div className="col" id="ColPaddingZero"><h2 id="BoxMonth1">LÅDANS VINER I JUNI</h2></div>
+                <div className="col" id="ColPaddingZero"><h2 id="BoxMonth3">LÅDANS VINER I JULI</h2></div>
+                <div className="col" id="ColPaddingZero"/>
             </div>
-            <a id="LinkTo1" href="/">Här kan du läsa om Maj lådan</a>
-            <div>
-                <img className="PlaceHolder1" scr="./Images/PlaceHolder.png" />
+            <div className="row" id="ContainerWindow">
+                <div className="col" id="ColPaddingZero"/>
+                <div className="col" id="ColPaddingZero"><img className="NextVine" scr="./Images/Maj.png"/></div>
+                <div className="col" id="ColPaddingZero"><img className="PlaceHolder1" scr="./Images/NewPlaceHolder.png"/></div>
+                <div className="col" id="ColPaddingZero"><img className="PlaceHolder2" scr="./Images/NewPlaceHolder.png"/></div>
+                <div className="col" id="ColPaddingZero"/>
             </div>
-            <a id="LinkTo2" href="/">Ej urvalt ännu</a>
-            <div>
-                <img className="PlaceHolder2" scr="./Images/PlaceHolder.png" />
+            <div className="row" id="ContainerWindow">
+                <div className="col" id="ColPaddingZero"/>
+                <div className="col" id="ColPaddingZero"><a id="LinkTo1" href="/">Här kan du läsa om Maj lådan!</a></div>
+                <div className="col" id="ColPaddingZero"><a id="LinkTo2" href="/">Ej urvalt ännu</a></div>
+                <div className="col" id="ColPaddingZero"><a id="LinkTo3" href="/">Ej urvalt ännu</a></div>
+                <div className="col" id="ColPaddingZero"/>
             </div>
-            <a id="LinkTo3" href="/">Ej urvalt ännu</a>
         </>
     )
-
 }
-export default Box4;
+
+export default Box4
