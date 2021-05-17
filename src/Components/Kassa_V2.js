638bc5930e2a44ef6aaa8184059afe254a755d7d
Dogginator <douglas.kvarnstrom91@gmail.com>
2021-05-17 13:12:21 +0200

    Kassa_new_Design

--- src/Components/Kassa_V2.js
+++ src/Components/Kassa_V2.js
@@ -1,7 +1,5 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
-const swish = "https://www.logo.wine/a/logo/Swish_(payment)/Swish_(payment)-Logo.wine.svg";
-const bankcard = "../Images/pngegg.png";
 const VinBox ="../Images/Vinbildconf.png";
 /* FUNKA FFS */
 
@@ -35,14 +33,49 @@ const Kassa_V2 = () => {
             console.log(responseFromAPI)
         });
     }
+    const [showPaymentCard, setShowPaymentCard] = useState(false);
+    const [showFaktura, setShowFaktura] = useState(false);
 
+    const cardClick = () =>{
+        setShowPaymentCard(true);
+        setShowFaktura(false);
+    }
+    const fakturaClick = () =>{
+        setShowFaktura(true);
+        setShowPaymentCard(false);
+    }
+
+    const PaymentCard = () =>{
+        return (
+            <div>
+                <div className="row">
+                    <div className="col"><input type="text" placeholder="Kortnummer"></input></div>
+                </div>
+            <div className="row">
+                <div className="col"><input type="text" placeholder="MM/ÅÅ"></input></div>
+                <div className="col"><input type="text" placeholder="CVC"></input></div>
+                </div> 
+            </div>
+
+        );
+    }
+    const Faktura = () =>{
+        return (
+            <div>
+
+            </div>
+
+        );
+    }
 
     return (
         <div className="row" id="ContainerWindow">
             <div className="col" id="KassaMain">
             <form onSubmit={(event) => submitForm(event)}>
                 <div className="row">
-                    <div className="col"><h1><b>Kassa</b></h1></div>
+                    <div className="col"/>
+                    <div className="col" id="KassaHeader"><h1><b>Kassa</b></h1></div>
+                    <div className="col"/>
                 </div>
                 <div className="row" >
                     <div className="col" id="CostumerInfoContainer">
@@ -114,23 +147,18 @@ const Kassa_V2 = () => {
                                 <div className="col"><h2>Betalningsätt</h2></div>
                             </div>
                             <div className="row">
-                                <div className="col"><input type="checkbox" id="Nyhetsbrev" name="Nyhetsbrev" />
-                                    <label for="Nyhetsbrev"> jag vill betal med kort</label>
+                                <div className="col"><input type="checkbox" id="Kort" name="Kort" onClick={cardClick}/>
+                                    <label for="Kort"> Kort</label>
                                 </div>
                             </div>
-                            <div className="row" id="cardContainer">
-                                <div className="row">
-                                    <div className="col"><input type="text" placeholder="Kortnummer"></input></div>
-                                </div>
-                                <div className="row">
-                                    <div className="col"><input type="text" placeholder="MM/ÅÅ"></input></div>
-                                    <div className="col"><input type="text" placeholder="CVC"></input></div>
-                                </div>
+                            <div className="row" id="Payment">
+                                {showFaktura && Faktura()}
+                                {showPaymentCard && PaymentCard()}
                             </div>
 
                             <div className="row">
-                                <div className="col"><input type="checkbox" id="Nyhetsbrev" name="Nyhetsbrev" />
-                                    <label for="Nyhetsbrev"> Jag vill ha faktura</label>
+                                <div className="col"><input type="checkbox" id="Faktura" name="Faktura" onClick={fakturaClick} />
+                                    <label for="Faktura"> Betala med faktura</label>
                                 </div>
                             </div>
                             
@@ -140,8 +168,41 @@ const Kassa_V2 = () => {
                 </div>
 
             </form>
+            <div className="row">
+                <div className="col">
+                    <Link>Återgå till varukorgen</Link>
+                </div>
+                <div className="col">
+                    <Link>
+                        <button>Bekrefta betalning</button>
+                    </Link>
+                    
+                </div>
+
+            </div>
             </div>
             <div className="col" id="VarukorgLittle">
+                <div className="row">
+                    <div className="col"><p>Översikt varukorg</p></div>
+                    <div className="col">
+                        <Link>Redigera</Link>
+                        </div>
+                </div>
+                <div className="row">
+                    <img alt="WineBox" src={VinBox} ></img>
+                </div>
+                <div className="row">
+                    <div className="col"><p>Delsumma</p></div>
+                    <div className="col"></div>
+                </div>
+                <div className="row">
+                    <div className="col"><p>Leverans</p></div>
+                    <div className="col"><p>0 SEK</p></div>
+                </div>
+                <div className="row">
+                    <div className="col"><p> Totalt att betala</p></div>
+                    <div className="col"></div>
+                </div>
 
             </div>
         </div>
